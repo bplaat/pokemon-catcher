8d0c21af5fcd5adcb679b170fb4bdb5c5bb06cac
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-06-21 10:45:55 +0200

    Fixed many bugs

--- client/js/game.js
+++ client/js/game.js
@@ -1,45 +1,53 @@
+// Utils
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 }
 
-let ws = undefined, pokemons = [], spawns = [], players = [];
-
+// Global state
+let ws = undefined, pokemons = [], spawns = [];
 function send(type, data) {
+    console.log('SEND', type, data);
     ws.send(JSON.stringify({ type, data }));
 }
 
+// Pokemon item component
 Vue.component('pokemon-item', {
     template: document.getElementById('pokemon-item-template').innerHTML,
-    props: [ 'pokemon' ],
+    props: [ 'player', 'pokemon' ],
     data() {
         return {
             isDeleting: false
         };
     },
     methods: {
+        revivePokemon() {
+            this.$parent.$emit('revivePokemon', this.pokemon.uniqueId);
+        },
         deletePokemon() {
             this.$parent.$emit('deletePokemon', this.pokemon.uniqueId);
         }
     }
 });
 
+// Main game component
 const app = new Vue({
     el: '#app',
     data: {
         page: 'connecting',
         connected: false,
         authed: false,
-        player: { id: uuidv4(), name: '', pokemons: [] },
+        player: { id: uuidv4(), name: '', admin: false, pokemons: [] },
+        otherPlayers: [],
         pokemonsMax: undefined
     },
 
     created() {
+        this.$on('revivePokemon', this.revivePokemon);
         this.$on('deletePokemon', this.deletePokemon);
         this.connect();
     },
@@ -47,9 +55,6 @@ const app = new Vue({
     watch: {
         player: {
             handler(player) {
-                if (this.authed) {
-                    send('player.update', { player });
-                }
                 localStorage.setItem('player', JSON.stringify(player));
             },
             deep: true
@@ -70,19 +75,19 @@ const app = new Vue({
         },
 
         onOpen() {
-            this.page = 'auth';
             this.connected = true;
             if (localStorage.player != null) {
+                this.page = 'game';
                 this.player = JSON.parse(localStorage.player);
                 send('player.connect', { player: this.player });
-                players.push(this.player);
-                this.page = 'game';
+            } else {
+                this.page = 'auth';
             }
         },
 
         onMessage(message) {
             const { type, data } = JSON.parse(message.data);
-            console.log(type, data);
+            console.log('RECEIVE', type, data);
 
             if (type == 'info') {
                 this.authed = true;
@@ -92,17 +97,24 @@ const app = new Vue({
             }
 
             if (type == 'player.connect') {
-                players.push(data.player);
+                this.otherPlayers.push(data.player);
             }
 
             if (type == 'player.update') {
-                const player = players.find(player => player.id == data.player.id);
-                player.name = data.player.name;
-                player.pokemons = data.player.pokemons;
+                if (this.player.id == data.player.id) {
+                    this.player.name = data.player.name;
+                    this.player.admin = data.player.admin;
+                    this.player.pokemons = data.player.pokemons;
+                } else {
+                    const otherPlayer = this.otherPlayers.find(player => player.id == data.player.id);
+                    otherPlayer.name = data.player.name;
+                    otherPlayer.admin = data.player.admin;
+                    otherPlayer.pokemons = data.player.pokemons;
+                }
             }
 
             if (type == 'player.disconnect') {
-                players = players.filter(player => player.id != data.player.id);
+                this.otherPlayers = this.otherPlayers.filter(player => player.id != data.player.id);
             }
         },
 
@@ -113,9 +125,8 @@ const app = new Vue({
 
         authPlayer() {
             if (this.player.name.length >= 2 && this.player.name.length <= 32) {
-                send('player.connect', { player: this.player });
-                players.push(this.player);
                 this.page = 'game';
+                send('player.connect', { player: this.player });
             }
         },
 
@@ -125,11 +136,41 @@ const app = new Vue({
                 pokemon.uniqueId = uuidv4();
                 pokemon.currentHealth = Math.floor(pokemon.health * (rand(1, 10) / 10));
                 this.player.pokemons.unshift(pokemon);
+                send('player.update', { player: this.player });
+            }
+        },
+
+        revivePokemon(uniqueId) {
+            const pokemon = this.player.pokemons.find(pokemon => pokemon.uniqueId == uniqueId);
+            if (pokemon != null) {
+                pokemon.currentHealth = pokemon.health;
+                send('player.update', { player: this.player });
+            } else if (this.player.admin) {
+                for (const otherPlayer of this.otherPlayers) {
+                    const pokemon = otherPlayer.pokemons.find(pokemon => pokemon.uniqueId == uniqueId);
+                    if (pokemon != null) {
+                        pokemon.currentHealth = pokemon.health;
+                        send('player.update', { player: otherPlayer });
+                    }
+                }
             }
         },
 
         deletePokemon(uniqueId) {
+            const pokemonsLength = this.player.pokemons.length;
             this.player.pokemons = this.player.pokemons.filter(pokemon => pokemon.uniqueId != uniqueId);
+            if (this.player.pokemons.length != pokemonsLength) {
+                send('player.update', { player: this.player });
+            } else if (this.player.admin) {
+                for (const otherPlayer of this.otherPlayers) {
+                    const pokemonsLength = otherPlayer.pokemons.length;
+                    otherPlayer.pokemons = otherPlayer.pokemons.filter(pokemon => pokemon.uniqueId != uniqueId);
+                    if (otherPlayer.pokemons.length != pokemonsLength) {
+                        send('player.update', { player: otherPlayer });
+                        break;
+                    }
+                }
+            }
         }
     }
 });
--- server/index.js
+++ server/index.js
@@ -20,15 +20,13 @@ const spawns = [
 const wss = new WebSocketServer({ noServer: true });
 let clients = [];
 wss.on('connection', ws => {
-    let player = undefined;
-
+    let playerId = undefined;
     function send(type, data) {
         ws.send(JSON.stringify({ type, data }));
     }
-
     function broadcast(type, data) {
         for (const client of clients) {
-            if (client.player.id != player.id) {
+            if (client.player.id != playerId) {
                 client.ws.send(JSON.stringify({ type, data }));
             }
         }
@@ -36,31 +34,39 @@ wss.on('connection', ws => {
 
     ws.on('message', message => {
         const { type, data } = JSON.parse(message.toString());
-        console.log('[WS]', type);
+        console.log('[WS]', playerId, type, data);
 
         if (type == 'player.connect') {
             const client = clients.find(client => client.player.id == data.player.id);
             if (client != null) client.ws.close();
 
-            player = data.player;
+            playerId = data.player.id;
             send('info', { pokemons, spawns, pokemonsMax: POKEMONS_MAX });
             for (const client of clients) {
+                console.log(client.player)
                 send('player.connect', { player: client.player });
             }
-            clients.push({ player, ws });
-            broadcast('player.connect', { player });
+            clients.push({ player: data.player, ws });
+            broadcast('player.connect', { player: data.player });
         }
 
         if (type == 'player.update') {
-            player = data.player;
-            broadcast('player.update', { player });
+            const client = clients.find(client => client.player.id == playerId);
+            const otherClient = clients.find(client => client.player.id == data.player.id);
+            if (otherClient == null) return;
+            if (!(data.player.id == playerId || client.player.admin)) return;
+
+            otherClient.player.name = data.player.name;
+            otherClient.player.admin = data.player.admin;
+            otherClient.player.pokemons = data.player.pokemons;
+            broadcast('player.update', { player: data.player });
         }
     });
 
     ws.on('close', () => {
-        if (player != undefined) {
-            clients = clients.filter(client => client.player.id != player.id);
-            broadcast('player.disconnect', { player: { id: player.id } });
+        if (playerId != undefined) {
+            clients = clients.filter(client => client.player.id != playerId);
+            broadcast('player.disconnect', { player: { id: playerId } });
         }
     });
 });
